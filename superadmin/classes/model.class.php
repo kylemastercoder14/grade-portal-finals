13bff3abfba83427d91604234d96cd9ca0f96d99
Kyle Andre David Lim <119409982+kylemastercoder14@users.noreply.github.com>
2024-04-23 12:59:18 +0800

    Update model.class.php

--- superadmin/classes/model.class.php
+++ superadmin/classes/model.class.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 class Model extends Dbconfig
 {
 
@@ -32,26 +34,33 @@ class Model extends Dbconfig
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($result) {
-            echo "<script>alert('EXISTING DATA!!!')</script>";
+            $_SESSION['message'] = "Progam already exist!";
+            $_SESSION['status'] = "#bb2124";
+            header("Location: programs.php");
         } else {
             $this->insert($data, $currentPage);
+            $_SESSION['message'] = "Program inserted successfully!";
+            $_SESSION['status'] = "#22bb33";
+            header("Location: programs.php");
         }
     }
     // call sa action
     public function callInsertProgram($data, $currentPage){
         $this->insertProgram($data, $currentPage);
     }
-    protected function readProgram(){
+    protected function readProgram($condition){
         $sql = "SELECT * FROM program_tbl WHERE is_archive = ?";
         $stmt = $this->db()->prepare($sql);
-        $stmt->execute([0]); // 0 is NOT  archive
+        $stmt->execute([$condition]); // 0 is NOT  archive
         $data = $stmt->fetchAll();
 
         return $data;
     }
-    public function getAllProgram(){
-        return $this->readProgram();
+    
+    public function getAllProgram($condition){
+        return $this->readProgram($condition);
     }
+
     protected function insert($data, $currentPage){
         $columns = implode(', ', array_keys($data));
         $placeholders = implode(', ', array_fill(0, count($data), '?'));
@@ -69,10 +78,14 @@ class Model extends Dbconfig
         }
 
         $stmt->execute();
+
+        return;
     }
+
     public function callEditProgram($data, $currentPage){
         $this->editProgram($data, $currentPage);
     }
+
     protected function editProgram($data, $currentPage){
         $tableName = $currentPage . '_tbl';
         $sql1 = "SELECT * FROM $tableName WHERE program_id = ?";
@@ -85,21 +98,41 @@ class Model extends Dbconfig
             $stmt = $this->db()->prepare($sql2);
             $stmt->execute([$data['program_name'],$data['program_code'],$data['program_id']]);
 
-            header('Location: programs.php');
+            $_SESSION['message'] = "Program updated successfully!";
+            $_SESSION['status'] = "#22bb33";
+            header("Location: programs.php");
             
         }else{
-            echo "error" . $data['program_name'],$data['program_code'],$data['program_id'];
+            $_SESSION['message'] = "Error updating program!";
+            $_SESSION['status'] = "#bb2124";
+            header("Location: programs.php");
         }
     }
     public function callArchiveProgram($data, $currentPage){
         $this->archiveProgram($data, $currentPage);
     }
+    protected function unarchiveProgram($program_id, $currentPage){
+        $tableName = $currentPage . '_tbl';
+        $sql = "UPDATE $tableName SET `is_archive` = ? WHERE `program_id` = ?";
+        $stmt = $this->db()->prepare($sql);
+        $stmt->execute([0, $program_id]); // 1 is archived, 0 is NOT
+
+        $_SESSION['message'] = "Program retrieved successfully!";
+        $_SESSION['status'] = "#22bb33";
+        header('Location: programs.php');
+    }
+    
+    public function callUnarchiveProgram($program_id,$currentPage) {
+        $this->unarchiveProgram($program_id, $currentPage);
+    }
     protected function archiveProgram($data, $currentPage){
         $tableName = $currentPage . '_tbl';
         $sql = "UPDATE $tableName SET `is_archive` = ? WHERE `program_id` = ?";
         $stmt = $this->db()->prepare($sql);
-        $success = $stmt->execute([1, $data['program_id']]); // 1 is archived, 0 is NOT
+        $stmt->execute([1, $data['program_id']]); // 1 is archived, 0 is NOT
 
+        $_SESSION['message'] = "Program archived successfully!";
+        $_SESSION['status'] = "#22bb33";
         header('Location: programs.php');
     }
 }
